obsproject/obs-studio

frontend: Add OBSSourceView widget

frontend/cmake/ui-widgets.cmake
@@ -55,6 +55,8 @@ target_sources(
     widgets/OBSProjector.hpp
     widgets/OBSQTDisplay.cpp
     widgets/OBSQTDisplay.hpp
+    widgets/OBSSourceView.cpp
+    widgets/OBSSourceView.hpp
     widgets/StatusBarWidget.cpp
     widgets/StatusBarWidget.hpp
     widgets/VolControl.cpp

Ryu0833/OBS-Ryu

frontend/cmake/ui-widgets.cmake
@@ -55,6 +55,8 @@ target_sources(
     widgets/OBSProjector.hpp
     widgets/OBSQTDisplay.cpp
     widgets/OBSQTDisplay.hpp
+    widgets/OBSSourceWidget.cpp
+    widgets/OBSSourceWidget.hpp
     widgets/StatusBarWidget.cpp
     widgets/StatusBarWidget.hpp
     widgets/VolControl.cpp