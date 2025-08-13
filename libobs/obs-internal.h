obsproject/obs-studio

libobs: Add assignment helper for async timestamps
Small helper function to ensure that the last system timestamp and the
last frame timestamp are updated together

libobs/obs-internal.h
@@ -791,7 +791,7 @@ struct obs_source {
 	uint64_t next_audio_ts_min;
 	uint64_t next_audio_sys_ts_min;
 	uint64_t last_frame_ts;
-	uint64_t last_sys_timestamp;
+	uint64_t last_ready_sys_ts;
 	bool async_rendered;
 
 	/* audio */
@@ -1073,6 +1073,7 @@ extern bool update_async_textures(struct obs_source *source, const struct obs_so
 				  gs_texture_t *tex[MAX_AV_PLANES], gs_texrender_t *texrender);
 extern bool set_async_texture_size(struct obs_source *source, const struct obs_source_frame *frame);
 extern void remove_async_frame(obs_source_t *source, struct obs_source_frame *frame);
+extern void set_ready_frame_timing(obs_source_t *source, uint64_t frame_ts, uint64_t sys_time);
 
 extern void set_deinterlace_texture_size(obs_source_t *source);
 extern void deinterlace_process_last_frame(obs_source_t *source, uint64_t sys_time);

libobs/obs-source-deinterlace.c
@@ -22,7 +22,7 @@ static bool ready_deinterlace_frames(obs_source_t *source, uint64_t sys_time)
 	struct obs_source_frame *next_frame = source->async_frames.array[0];
 	struct obs_source_frame *prev_frame = NULL;
 	struct obs_source_frame *frame = NULL;
-	uint64_t sys_offset = sys_time - source->last_sys_timestamp;
+	uint64_t sys_offset = sys_time - source->last_ready_sys_ts;
 	uint64_t frame_time = next_frame->timestamp;
 	uint64_t frame_offset = 0;
 	size_t idx = 1;
@@ -50,18 +50,18 @@ static bool ready_deinterlace_frames(obs_source_t *source, uint64_t sys_time)
 			source->async_frames.array[0]->prev_frame = prev_frame;
 		}
 		source->deinterlace_offset = 0;
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, sys_time);
 		return true;
 	}
 
 	/* account for timestamp invalidation */
 	if (frame_out_of_bounds(source, frame_time)) {
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, source->last_ready_sys_ts);
 		source->deinterlace_offset = 0;
 		return true;
 	} else {
 		frame_offset = frame_time - source->last_frame_ts;
-		source->last_frame_ts += sys_offset;
+		set_ready_frame_timing(source, source->last_frame_ts + sys_offset, source->last_ready_sys_ts);
 	}
 
 	while (source->last_frame_ts > next_frame->timestamp) {
@@ -105,7 +105,7 @@ static bool ready_deinterlace_frames(obs_source_t *source, uint64_t sys_time)
 
 		/* more timestamp checking and compensating */
 		if ((next_frame->timestamp - frame_time) > MAX_TS_VAR) {
-			source->last_frame_ts = next_frame->timestamp - frame_offset;
+			set_ready_frame_timing(source, next_frame->timestamp - frame_offset, source->last_ready_sys_ts);
 			source->deinterlace_offset = 0;
 		}
 
@@ -181,7 +181,7 @@ static inline void deinterlace_get_closest_frames(obs_source_t *s, uint64_t sys_
 		}
 
 		if (!s->last_frame_ts)
-			s->last_frame_ts = s->cur_async_frame->timestamp;
+			set_ready_frame_timing(s, s->cur_async_frame->timestamp, s->last_ready_sys_ts);
 
 		s->deinterlace_frame_ts = s->cur_async_frame->timestamp;
 

libobs/obs-source.c
@@ -1234,7 +1234,7 @@ static void async_tick(obs_source_t *source)
 		source->cur_async_frame = get_closest_frame(source, sys_time);
 	}
 
-	source->last_sys_timestamp = sys_time;
+	set_ready_frame_timing(source, source->last_frame_ts, sys_time);
 
 	if (deinterlacing_enabled(source))
 		filter_frame(source, &source->prev_async_frame);
@@ -3403,6 +3403,7 @@ static inline struct obs_source_frame *cache_video(struct obs_source *source, co
 	if (source->async_frames.num >= MAX_ASYNC_FRAMES) {
 		free_async_cache(source);
 		source->last_frame_ts = 0;
+		source->last_ready_sys_ts = 0;
 		pthread_mutex_unlock(&source->async_mutex);
 		return NULL;
 	}
@@ -3461,6 +3462,7 @@ static void obs_source_output_video_internal(obs_source_t *source, const struct 
 		pthread_mutex_lock(&source->async_mutex);
 		source->async_active = false;
 		source->last_frame_ts = 0;
+		source->last_ready_sys_ts = 0;
 		free_async_cache(source);
 		pthread_mutex_unlock(&source->async_mutex);
 		return;
@@ -3606,7 +3608,7 @@ static void obs_source_preload_video_internal(obs_source_t *source, const struct
 
 	copy_frame_data(source->async_preload_frame, frame);
 
-	source->last_frame_ts = frame->timestamp;
+	set_ready_frame_timing(source, frame->timestamp, obs->video.video_time);
 }
 
 void obs_source_preload_video(obs_source_t *source, const struct obs_source_frame *frame)
@@ -3702,7 +3704,7 @@ static void obs_source_set_video_frame_internal(obs_source_t *source, const stru
 	set_async_texture_size(source, source->async_preload_frame);
 	update_async_textures(source, source->async_preload_frame, source->async_textures, source->async_texrender);
 
-	source->last_frame_ts = frame->timestamp;
+	set_ready_frame_timing(source, frame->timestamp, obs->video.video_time);
 
 	obs_leave_graphics();
 }
@@ -3976,11 +3978,19 @@ void remove_async_frame(obs_source_t *source, struct obs_source_frame *frame)
 
 /* #define DEBUG_ASYNC_FRAMES 1 */
 
+void set_ready_frame_timing(obs_source_t *source, uint64_t frame_ts, uint64_t sys_time)
+{
+	/* the timestamp provided by the source for the 'readied' frame */
+	source->last_frame_ts = frame_ts;
+	/* the corresponding system (video) timestamp given the source timestamp */
+	source->last_ready_sys_ts = sys_time;
+}
+
 static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 {
 	struct obs_source_frame *next_frame = source->async_frames.array[0];
 	struct obs_source_frame *frame = NULL;
-	uint64_t sys_offset = sys_time - source->last_sys_timestamp;
+	uint64_t sys_offset = sys_time - source->last_ready_sys_ts;
 	uint64_t frame_time = next_frame->timestamp;
 	uint64_t frame_offset = 0;
 
@@ -3991,7 +4001,7 @@ static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 			next_frame = source->async_frames.array[0];
 		}
 
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, sys_time);
 		return true;
 	}
 
@@ -4009,11 +4019,11 @@ static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 #if DEBUG_ASYNC_FRAMES
 		blog(LOG_DEBUG, "timing jump");
 #endif
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, source->last_ready_sys_ts);
 		return true;
 	} else {
 		frame_offset = frame_time - source->last_frame_ts;
-		source->last_frame_ts += sys_offset;
+		set_ready_frame_timing(source, source->last_frame_ts + sys_offset, source->last_ready_sys_ts);
 	}
 
 	while (source->last_frame_ts > next_frame->timestamp) {
@@ -4049,7 +4059,7 @@ static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 #if DEBUG_ASYNC_FRAMES
 			blog(LOG_DEBUG, "timing jump");
 #endif
-			source->last_frame_ts = next_frame->timestamp - frame_offset;
+			set_ready_frame_timing(source, next_frame->timestamp - frame_offset, source->last_ready_sys_ts);
 		}
 
 		frame_time = next_frame->timestamp;
@@ -4074,7 +4084,7 @@ static inline struct obs_source_frame *get_closest_frame(obs_source_t *source, u
 		da_erase(source->async_frames, 0);
 
 		if (!source->last_frame_ts)
-			source->last_frame_ts = frame->timestamp;
+			set_ready_frame_timing(source, frame->timestamp, source->last_ready_sys_ts);
 
 		return frame;
 	}

Ryu0833/OBS-Ryu

libobs/obs-internal.h
@@ -791,7 +791,7 @@ struct obs_source {
 	uint64_t next_audio_ts_min;
 	uint64_t next_audio_sys_ts_min;
 	uint64_t last_frame_ts;
-	uint64_t last_sys_timestamp;
+	uint64_t last_frame_sys_ts;
 	bool async_rendered;
 
 	/* audio */
@@ -1073,6 +1073,7 @@ extern bool update_async_textures(struct obs_source *source, const struct obs_so
 				  gs_texture_t *tex[MAX_AV_PLANES], gs_texrender_t *texrender);
 extern bool set_async_texture_size(struct obs_source *source, const struct obs_source_frame *frame);
 extern void remove_async_frame(obs_source_t *source, struct obs_source_frame *frame);
+extern void set_ready_frame_timing(obs_source_t *source, uint64_t frame_ts, uint64_t sys_time);
 
 extern void set_deinterlace_texture_size(obs_source_t *source);
 extern void deinterlace_process_last_frame(obs_source_t *source, uint64_t sys_time);

libobs/obs-source-deinterlace.c
@@ -22,7 +22,7 @@ static bool ready_deinterlace_frames(obs_source_t *source, uint64_t sys_time)
 	struct obs_source_frame *next_frame = source->async_frames.array[0];
 	struct obs_source_frame *prev_frame = NULL;
 	struct obs_source_frame *frame = NULL;
-	uint64_t sys_offset = sys_time - source->last_sys_timestamp;
+	uint64_t sys_offset = sys_time - source->last_frame_sys_ts;
 	uint64_t frame_time = next_frame->timestamp;
 	uint64_t frame_offset = 0;
 	size_t idx = 1;
@@ -50,18 +50,18 @@ static bool ready_deinterlace_frames(obs_source_t *source, uint64_t sys_time)
 			source->async_frames.array[0]->prev_frame = prev_frame;
 		}
 		source->deinterlace_offset = 0;
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, sys_time);
 		return true;
 	}
 
 	/* account for timestamp invalidation */
 	if (frame_out_of_bounds(source, frame_time)) {
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, source->last_frame_sys_ts);
 		source->deinterlace_offset = 0;
 		return true;
 	} else {
 		frame_offset = frame_time - source->last_frame_ts;
-		source->last_frame_ts += sys_offset;
+		set_ready_frame_timing(source, source->last_frame_ts + sys_offset, source->last_frame_sys_ts);
 	}
 
 	while (source->last_frame_ts > next_frame->timestamp) {
@@ -105,7 +105,7 @@ static bool ready_deinterlace_frames(obs_source_t *source, uint64_t sys_time)
 
 		/* more timestamp checking and compensating */
 		if ((next_frame->timestamp - frame_time) > MAX_TS_VAR) {
-			source->last_frame_ts = next_frame->timestamp - frame_offset;
+			set_ready_frame_timing(source, next_frame->timestamp - frame_offset, source->last_frame_sys_ts);
 			source->deinterlace_offset = 0;
 		}
 
@@ -181,7 +181,7 @@ static inline void deinterlace_get_closest_frames(obs_source_t *s, uint64_t sys_
 		}
 
 		if (!s->last_frame_ts)
-			s->last_frame_ts = s->cur_async_frame->timestamp;
+			set_ready_frame_timing(s, s->cur_async_frame->timestamp, s->last_frame_sys_ts);
 
 		s->deinterlace_frame_ts = s->cur_async_frame->timestamp;
 

libobs/obs-source.c
@@ -1234,7 +1234,7 @@ static void async_tick(obs_source_t *source)
 		source->cur_async_frame = get_closest_frame(source, sys_time);
 	}
 
-	source->last_sys_timestamp = sys_time;
+	set_ready_frame_timing(source, source->last_frame_ts, sys_time);
 
 	if (deinterlacing_enabled(source))
 		filter_frame(source, &source->prev_async_frame);
@@ -3403,6 +3403,7 @@ static inline struct obs_source_frame *cache_video(struct obs_source *source, co
 	if (source->async_frames.num >= MAX_ASYNC_FRAMES) {
 		free_async_cache(source);
 		source->last_frame_ts = 0;
+		source->last_frame_sys_ts = 0;
 		pthread_mutex_unlock(&source->async_mutex);
 		return NULL;
 	}
@@ -3461,6 +3462,7 @@ static void obs_source_output_video_internal(obs_source_t *source, const struct 
 		pthread_mutex_lock(&source->async_mutex);
 		source->async_active = false;
 		source->last_frame_ts = 0;
+		source->last_frame_sys_ts = 0;
 		free_async_cache(source);
 		pthread_mutex_unlock(&source->async_mutex);
 		return;
@@ -3606,7 +3608,7 @@ static void obs_source_preload_video_internal(obs_source_t *source, const struct
 
 	copy_frame_data(source->async_preload_frame, frame);
 
-	source->last_frame_ts = frame->timestamp;
+	set_ready_frame_timing(source, frame->timestamp, obs->video.video_time);
 }
 
 void obs_source_preload_video(obs_source_t *source, const struct obs_source_frame *frame)
@@ -3702,7 +3704,7 @@ static void obs_source_set_video_frame_internal(obs_source_t *source, const stru
 	set_async_texture_size(source, source->async_preload_frame);
 	update_async_textures(source, source->async_preload_frame, source->async_textures, source->async_texrender);
 
-	source->last_frame_ts = frame->timestamp;
+	set_ready_frame_timing(source, frame->timestamp, obs->video.video_time);
 
 	obs_leave_graphics();
 }
@@ -3976,11 +3978,19 @@ void remove_async_frame(obs_source_t *source, struct obs_source_frame *frame)
 
 /* #define DEBUG_ASYNC_FRAMES 1 */
 
+void set_ready_frame_timing(obs_source_t *source, uint64_t frame_ts, uint64_t sys_time)
+{
+	/* the timestamp provided by the source for the 'readied' frame */
+	source->last_frame_ts = frame_ts;
+	/* the corresponding system (video) timestamp given the source timestamp */
+	source->last_frame_sys_ts = sys_time;
+}
+
 static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 {
 	struct obs_source_frame *next_frame = source->async_frames.array[0];
 	struct obs_source_frame *frame = NULL;
-	uint64_t sys_offset = sys_time - source->last_sys_timestamp;
+	uint64_t sys_offset = sys_time - source->last_frame_sys_ts;
 	uint64_t frame_time = next_frame->timestamp;
 	uint64_t frame_offset = 0;
 
@@ -3991,7 +4001,7 @@ static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 			next_frame = source->async_frames.array[0];
 		}
 
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, sys_time);
 		return true;
 	}
 
@@ -4009,11 +4019,11 @@ static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 #if DEBUG_ASYNC_FRAMES
 		blog(LOG_DEBUG, "timing jump");
 #endif
-		source->last_frame_ts = next_frame->timestamp;
+		set_ready_frame_timing(source, next_frame->timestamp, source->last_frame_sys_ts);
 		return true;
 	} else {
 		frame_offset = frame_time - source->last_frame_ts;
-		source->last_frame_ts += sys_offset;
+		set_ready_frame_timing(source, source->last_frame_ts + sys_offset, source->last_frame_sys_ts);
 	}
 
 	while (source->last_frame_ts > next_frame->timestamp) {
@@ -4049,7 +4059,7 @@ static bool ready_async_frame(obs_source_t *source, uint64_t sys_time)
 #if DEBUG_ASYNC_FRAMES
 			blog(LOG_DEBUG, "timing jump");
 #endif
-			source->last_frame_ts = next_frame->timestamp - frame_offset;
+			set_ready_frame_timing(source, next_frame->timestamp - frame_offset, source->last_frame_sys_ts);
 		}
 
 		frame_time = next_frame->timestamp;
@@ -4074,7 +4084,7 @@ static inline struct obs_source_frame *get_closest_frame(obs_source_t *source, u
 		da_erase(source->async_frames, 0);
 
 		if (!source->last_frame_ts)
-			source->last_frame_ts = frame->timestamp;
+			set_ready_frame_timing(source, frame->timestamp, source->last_frame_sys_ts);
 
 		return frame;
 	}