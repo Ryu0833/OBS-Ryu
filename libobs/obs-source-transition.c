obsproject/obs-studio

libobs: Add obs_is_transition_active

libobs/obs-source-transition.c
@@ -319,6 +319,11 @@ static inline bool transition_active(obs_source_t *transition)
 	return transition->transitioning_audio || transition->transitioning_video;
 }
 
+bool obs_is_transition_active(obs_source_t *transition)
+{
+	return transition_active(transition);
+}
+
 bool obs_transition_start(obs_source_t *transition, enum obs_transition_mode mode, uint32_t duration_ms,
 			  obs_source_t *dest)
 {

libobs/obs.h
@@ -1604,6 +1604,8 @@ EXPORT uint32_t obs_transition_get_alignment(const obs_source_t *transition);
 EXPORT void obs_transition_set_size(obs_source_t *transition, uint32_t cx, uint32_t cy);
 EXPORT void obs_transition_get_size(const obs_source_t *transition, uint32_t *cx, uint32_t *cy);
 
+EXPORT bool obs_is_transition_active(obs_source_t *transition);
+
 /* function used by transitions */
 
 /**

Ryu0833/OBS-Ryu

libobs/obs-source-transition.c
@@ -319,6 +319,11 @@ static inline bool transition_active(obs_source_t *transition)
 	return transition->transitioning_audio || transition->transitioning_video;
 }
 
+bool obs_transition_is_active(obs_source_t *transition)
+{
+	return transition_active(transition);
+}
+
 bool obs_transition_start(obs_source_t *transition, enum obs_transition_mode mode, uint32_t duration_ms,
 			  obs_source_t *dest)
 {

libobs/obs.h
@@ -1604,6 +1604,8 @@ EXPORT uint32_t obs_transition_get_alignment(const obs_source_t *transition);
 EXPORT void obs_transition_set_size(obs_source_t *transition, uint32_t cx, uint32_t cy);
 EXPORT void obs_transition_get_size(const obs_source_t *transition, uint32_t *cx, uint32_t *cy);
 
+EXPORT bool obs_transition_is_active(obs_source_t *transition);
+
 /* function used by transitions */
 
 /**