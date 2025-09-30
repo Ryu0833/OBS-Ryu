gxalpha/obs-studio

frontend,shared: Replace and disable QByteArray to char * casts

frontend/OBSApp.cpp
@@ -688,7 +688,7 @@ bool LoadBranchesFile(vector<UpdateBranch> &out)
 		goto fail;
 	}
 
-	branchesText = branchesFile.readAll();
+	branchesText = branchesFile.readAll().constData();
 	if (branchesText.empty()) {
 		error = "File empty.";
 		goto fail;

Ryu0833/OBS-Ryu

frontend/OBSApp.cpp
@@ -802,7 +802,7 @@ bool LoadBranchesFile(vector<UpdateBranch> &out)
 		goto fail;
 	}
 
-	branchesText = branchesFile.readAll();
+	branchesText = branchesFile.readAll().toStdString();
 	if (branchesText.empty()) {
 		error = "File empty.";
 		goto fail;